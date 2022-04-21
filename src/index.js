03754a6e46c748a16eac99f2e1fd6247ab1228aa
miraykaymak <miirayazz00@gmail.com>
2022-04-22 00:13:42 +0300

    Dispatch Action

--- src/index.js
+++ src/index.js
@@ -4,17 +4,44 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import AppRouter from "./routers/AppRouter";
 import "./App.css";
-import {createStore} from 'redux';
+import { createStore } from "redux";
+import { type } from "@testing-library/user-event/dist/type";
 
 const initialState = {
-    count:0
-}
+  count: 0,
+};
 
-const store = ((state = initialState)=>{
-    return state
-})
+const store = (state = initialState, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      const incrementBy =
+        typeof action.incrementBy === "number" ? action.incrementBy : 1;
+      return {
+        count: state.count + incrementBy,
+      };
+    case "DECREMENT":
+      return {
+        count: state.count - 1,
+      };
+    case "RESET":
+      return {
+        count: 0,
+      };
+    default:
+      return state;
+  }
+};
 
 console.log(store.getState());
+
+// store.dispatch({
+//   type: "INCREMENT",
+//   incrementBy: 10,
+// });
+
+// store.dispatch({
+//   type: "DECREMENT",
+// });
 
 ReactDOM.render(<AppRouter />, document.getElementById("root"));
 
