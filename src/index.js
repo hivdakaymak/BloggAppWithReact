042918720ecae16d6a2561db1031be1a08370060
miraykaymak <miirayazz00@gmail.com>
2022-04-21 08:20:12 +0300

    Redux Setup

--- src/index.js
+++ src/index.js
@@ -3,7 +3,18 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import AppRouter from "./routers/AppRouter";
-import "./App.css"
+import "./App.css";
+import {createStore} from 'redux';
+
+const initialState = {
+    count:0
+}
+
+const store = ((state = initialState)=>{
+    return state
+})
+
+console.log(store.getState());
 
 ReactDOM.render(<AppRouter />, document.getElementById("root"));
 
