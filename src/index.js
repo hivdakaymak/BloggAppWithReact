2b586369250c4a9b4f5aee7d52d750169ab8c856
miraykaymak <miirayazz00@gmail.com>
2022-04-22 15:36:26 +0300

    add state redux

--- src/index.js
+++ src/index.js
@@ -5,44 +5,65 @@ import "./App.css";
 import { createStore, combineReducers } from "redux";
 
 const state = {
-    blogs:[{
-        id: 1,
-        title: 'Blog Title 1',
-        description : "Blog Description",
-        dateAdded: 0
-    }],
-    auth:{
-        userid: 1,
-        username: 'miray',
-        email:'miirayazz@gmail.com'
-    }
-}
-
-const blogState = []
-
-const blogReducer = (state = blogState,action) =>{ 
-    switch (action.type){
-        default:
-            return state
-    }
-
-}
-
-const authState =  auth:{
+  blogs: [
+    {
+      id: 1,
+      title: "Blog Title 1",
+      description: "Blog Description",
+      dateAdded: 0,
+    },
+  ],
+  auth: {
     userid: 1,
-    username: 'miray',
-    email:'miirayazz@gmail.com'
+    username: "miray",
+    email: "miirayazz@gmail.com",
+  },
+};
+
+const addBlog = ({title='', description='', dateAdded=''}) => ({
+  type: "ADD_BLOG",
+  blog: {
+    id: 2,
+    title: "aa",
+    description: "bb",
+    dateAdded: 0,
+  },
+});
+
+const blogState = [];
+
+const blogReducer = (state = blogState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+const authState = {
+  auth: {
+    userid: 1,
+    username: "miray",
+    email: "miirayazz@gmail.com",
+  },
 };
 
 const authReducer = (state = authState, action) => {
-    switch (action.type){
-        default:
-            return state
-    }
-}
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
 
+const store = createStore(
+  combineReducers({
+    blogs: blogReducer,
+    auth: authReducer,
+  })
+);
 
-const store = createStore(authReducer)
+store.subscribe(() => {
+  console.log(store.getState());
+});
 
 ReactDOM.render(<AppRouter />, document.getElementById("root"));
 
