a5045832f1f29998fe463c929c4b8698a31ce425
miraykaymak <miirayazz00@gmail.com>
2022-04-26 19:17:01 +0300

    deleted

--- src/compenents/EditBlogPage.js
+++ src/compenents/EditBlogPage.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import BlogForm from './BlogForm';
 import { connect } from 'react-redux';
+import { editBlog,removeBlog } from '../actions/blogs';
 const EditBlogPage = () => {
   return (
     <div>
       <h1>Edit Page</h1> 
-      <BlogForm blog={props.blog} /> 
+      <BlogForm
+       blog={props.blog} 
+       onSubmit={(blog) => {
+           props.dispatch(editBlog(props.blog.id, blog));
+           props.history.push('/blogs')
+       }}
+       /> 
+       <button onClick={() =>{
+           props.dispatch(removeBlog({id:props.blog.id}));
+           props.history.push('/blogs');
+       }}>Delete</button>
     </div>
   );
 }
